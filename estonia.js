6a5c52ad2e43b31ed560c7937a29746288c94a2f
kabir <143556475+k-abir@users.noreply.github.com>
2023-08-29 15:40:20 -0700

    Create estonia.js

---
+++ estonia.js
@@ -0,0 +1,13 @@
+function setup() {
+  createCanvas(600, 300);
+}
+
+function draw() {
+  background(0, 114, 206);
+  const c = color((10, 19, 11));
+  fill(c);
+  rect(0,100,600,100)
+  const b = color((255,255,255));
+  fill(b);
+  rect(0,200,700,100)
+}
