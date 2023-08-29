f6b3b70f960e0dda603ababd6c20a3d97171ae94
kabir <143556475+k-abir@users.noreply.github.com>
2023-08-29 15:41:52 -0700

    Update estonia.js

--- estonia.js
+++ estonia.js
@@ -1,13 +1,19 @@
+
 function setup() {
   createCanvas(600, 300);
 }
 
 function draw() {
+  //set blue background
   background(0, 114, 206);
+  //create black color
   const c = color((10, 19, 11));
+  //fill next rectangle with color
   fill(c);
   rect(0,100,600,100)
+  //create white color
   const b = color((255,255,255));
+  //fill next rectangle with color
   fill(b);
   rect(0,200,700,100)
 }
